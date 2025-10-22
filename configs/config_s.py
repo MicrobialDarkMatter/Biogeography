70db9680dc13d804848e536b574209a9a1e34963
ThomasHeede <145970483+ThomasHeede@users.noreply.github.com>
2025-10-22 14:19:04 +0200

    Notebook to solve spatial issue

--- configs/config_s.py
+++ configs/config_s.py
@@ -1,39 +1,40 @@
 import os
 import torch
 
+# from configs.data_folder_path import data_folder_path
 from configs.base_path import base_path
 from EcoGP.likelihoods import DirichletMultinomialLikelihood, BernoulliLikelihood
 
 config = {
     "data": {
-        "X_path": os.path.join(base_path, "data/butterfly/X_val.csv"),
-        "Y_path": os.path.join(base_path, "data/butterfly/Y_val.csv"),
-        "coords_path": os.path.join(base_path, "data/butterfly/XY_val.csv"),
-        "traits_path": "",#os.path.join(base_path, "data/butterfly/traits.csv"),
+        "X_path": os.path.join(base_path, "data/clean/X.csv"),
+        "Y_path": os.path.join(base_path, "data/clean/Y.csv"),
+        "coords_path": os.path.join(base_path, "data/clean/XY.csv"),
+        "traits_path": "",#os.path.join(base_path, "data/clean/traits.csv"),
         "normalize_X": True,
         "prevalence_threshold": 0.0,
-        "total_counts_path": "",
+        "total_counts_path": "",#os.path.join(base_path, "data/clean/total_counts.csv"),
         "presence_absence": True,
     },
     "general": {
         "likelihood": BernoulliLikelihood,
-        "n_iter": 200,
+        "n_iter": 100,
         "n_particles": 1,
-        "lr": 0.0025,
+        "lr": 0.005,
         "batch_size": 256,
         "split_pct": [0.7, 0.2, 0.1],  # Train/Test/Val
         "device": torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
         "verbose": True,
         "save_model_path": os.path.join(base_path, "results/saved_models/"),
-        "seed": 0,
+        "seed": 1,
     },
     "environmental": {
         "n_latents": 10,
-        "n_inducing_points": 200,
+        "n_inducing_points": 50,
     },
     "spatial": {
-        "n_latents": 10,
-        "n_inducing_points": 50,
+        "n_latents": 5,
+        "n_inducing_points": 200,
     },
     "hmsc": {
         "k_folds": 5,
